79fb3465654b977beaf61a9407db309900817f5f
Umar K <umar-khan@users.noreply.github.com>
2016-11-13 01:44:27 -0500

    Setup basic search and random search API calls

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
 $(document).ready(function () {
   
-  var searchResults = "";
-  var randomResults = "";
+  var searchResults = [];
+  var random;
   var input = "";
   
   function search() {
-    $.getJSON("http://en.wikipedia.org/w/api.php/w/api.php?action=query&format=json&list=search&srsearch=butterflies&callback=?", function (json) {
-      searchResults = JSON.stringify(json);
-      console.log(searchResults);
-      $("#searchResults").html(searchResults);
+    // Use Wikipedia API to return 10 search results.
+    // Results are returned as an array
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=game&callback=?", function (json) {
+      
+      // Store search results in searchResults array
+      searchResults = json;
+      
+      // Call function to show results
+      showResults();
+      
     });
   }
   
-  /*
-  function random() {
-    $.getJSON("", function (json) {
-      randomResults = JSON.stringify(json);
-      $("#randomResults").html(searchResults);
+  
+  function randomSearch() {
+    // Uses Wikipedia API to return a random article
+    // Results are returned as a JSON object
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=random&exchars=100&explaintext=1&inprop=url&grnnamespace=0&callback=?", function (json) {
+      
+      random = JSON.stringify(json);
+      
+      // Call function to show results
+      showResults();
+      
     });
   }
-  */
+  
+  function showResults() {
+    
+    $("#searchResults").html(searchResults);
+    $("#randomResults").html(random);
+    
+  }
+  
   
   search();
+  randomSearch();
 });
+
+
