b5f99a575926567f17e7fefbd41aec5e4621528f
Umar K <umar-khan@users.noreply.github.com>
2016-11-13 19:42:54 -0500

    Clean up UI and complete results processing Show results in one div based on hidden template Make results show up in one div Make results overwrite existing results Process API results and pass as arrays to display function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 $(document).ready(function () {
   
-  var searchResults = [];
-  var random = "";
-  var input = "";
-  
   function search(query) {
     // Use Wikipedia API to return 10 search results based on input
     // Results are returned as an array
     $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=" + query, function (json) {
       
-      // Store search results in searchResults array
-      searchResults = json;
-      console.log(query);
+      var titles = [];
+      var descriptions = [];
+      var urls = [];
       
+      // Store article Titles, Descriptions, and URLs, in arrays
+      titles = json[1];
+      descriptions = json[2];
+      urls = json[3];
+      
+      // Build output array of results
+      output = [titles, descriptions, urls];
+
       // Call function to show results
-      showResults();
+      showResults(output);
       
     });
   }
@@ -23,20 +27,49 @@ $(document).ready(function () {
   function randomSearch() {
     // Uses Wikipedia API to return a random article
     // Results are returned as a JSON object
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=random&exchars=100&explaintext=1&inprop=url&grnnamespace=0&callback=?", function (json) {
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=random&redirects=1&exsentences=2&explaintext=1&inprop=url&grnnamespace=0&callback=?", function (json) {
       
-      random = JSON.stringify(json);
+      var title = [];
+      var description = [];
+      var url = [];
       
+      // The API seems to return a Javascript Object instead of JSON
+      // We have to find the dynamically generated page ID in the path
+      var pageID = Object.keys(json["query"]["pages"])[0];
+      
+      // Store the random article Title, Description, and URL, in arrays
+      title.push(json["query"]["pages"][pageID]["title"]);
+      description.push(json["query"]["pages"][pageID]["extract"]);
+      url.push(json["query"]["pages"][pageID]["fullurl"]);
+      
+      // Build output array of results
+      var output = [title, description, url];
+
       // Call function to show results
-      showResults();
+      showResults(output);
       
     });
   }
   
-  function showResults() {
+  function showResults(resultsArray) {
     
-    $("#searchResults").html(searchResults);
-    $("#randomResults").html(random);
+    // Remove any visible result items
+    $(".listItem").remove();
+    
+    //
+    for (var i = 0; i < resultsArray[0].length; i++) {
+      var listItem = $(".template").clone();
+      listItem.removeClass("template");
+      listItem.addClass("listItem");
+      
+      // Set Title, Desctiption, and URL
+      listItem.find("h4").text(resultsArray[0][i]);
+      listItem.find("p").text(resultsArray[1][i]);
+      listItem.attr("href", resultsArray[2][i]);
+      
+      // Add new list item to DOM
+      $("#results").append(listItem);
+    }
     
   }
   
